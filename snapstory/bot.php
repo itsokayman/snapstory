980b2407751978e706d29d6c8bdf3215a133fd94
Sean Hamill <seanhamill@Seans-Air.lan>
2015-05-14 22:00:43 +0200

    debug on by default

--- snapstory/bot.php
+++ snapstory/bot.php
@@ -6,7 +6,7 @@ $username = 'USERNAME'; // Your snapchat username
 $password = 'PASSWORD'; // Your snapchat password
 $gEmail   = 'GMAIL@GMAIL.COM'; // Gmail account
 $gPasswd  = 'APP_PASSWORD'; // Gmail account password
-$debug = false; // Set this to true if you want to see all outgoing requests and responses from server
+$debug = true; // Set this to true if you want to see all outgoing requests and responses from server
 $addback = true;
 ////////////////////////////////
 
